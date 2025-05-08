d7fb761e12cff54b0d1dafa0fc402b065de34332
bhamutci <bhamutcu@hotmail.com>
2025-05-08 14:12:57 +0100

    Add EntityFramework and postgres entityframework nuget packages

--- src/FinaTech.EntityFramework/obj/Debug/net9.0/FinaTech.EntityFramework.AssemblyInfo.cs
+++ src/FinaTech.EntityFramework/obj/Debug/net9.0/FinaTech.EntityFramework.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinaTech.EntityFramework")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53d8486b312da7fc617419ec063602c67c9c09e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinaTech.EntityFramework")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinaTech.EntityFramework")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
