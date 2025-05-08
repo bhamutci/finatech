9804bfa5beadb677859a53fffb42aa3f5aac4926
bhamutci <bhamutcu@hotmail.com>
2025-05-08 21:57:46 +0100

    Remove bin and obj directories from tracking

---
+++ src/FinaTech.Application/ApplicationModule.cs
@@ -0,0 +1,29 @@
+namespace FinaTech.Application;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PaymentService;
+using EntityFramework;
+
+/// <summary>
+/// Provides extension methods for configuring application-specific services in the dependency injection container.
+/// </summary>
+public static class ApplicationModule
+{
+    /// <summary>
+    /// Adds application-specific services to the dependency injection container.
+    /// </summary>
+    /// <param name="services">The collection of service descriptors.</param>
+    /// <param name="configuration">The configuration object used to configure services.</param>
+    /// <returns>The updated service collection.</returns>
+    public static IServiceCollection AddApplication(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddEntityFramework(configuration);
+
+        services.AddScoped<PaymentStrategy, SEPAPayment>();
+        services.AddScoped<PaymentStrategy, SWIFTPayment>();
+        services.AddScoped<IPaymentService, PaymentService.PaymentService>();
+
+        return services;
+    }
+}
---
+++ src/FinaTech.EntityFramework/EntityFrameworkModule.cs
@@ -0,0 +1,40 @@
+namespace FinaTech.EntityFramework;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PostgresSqlServer;
+
+/// <summary>
+/// Provides an extension method for configuring and adding Entity Framework support
+/// to the application's dependency injection container.
+/// </summary>
+/// <remarks>
+/// This module integrates with Microsoft.EntityFrameworkCore and utilizes PostgreSQL
+/// as the underlying database. It sets up the DbContext with a connection string
+/// retrieved from IConfiguration and includes additional properties for context-specific behavior.
+/// </remarks>
+public static class EntityFrameworkModule
+{
+    /// <summary>
+    /// Configures and adds Entity Framework Core support to the application's dependency injection container
+    /// using PostgreSQL as the database provider.
+    /// </summary>
+    /// <param name="services">The <see cref="IServiceCollection"/> to add services to.</param>
+    /// <param name="configuration">The application configuration, providing access to the connection string.</param>
+    /// <returns>
+    /// The updated <see cref="IServiceCollection"/> instance to allow for method chaining.
+    /// </returns>
+    public static IServiceCollection AddEntityFramework(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        services.AddDbContext<FinaTechPostgresSqlDbContext>(builder =>
+        {
+            builder.EnableSensitiveDataLogging();
+            builder.UseNpgsql(connectionString);
+        });
+
+        return services;
+    }
+}
