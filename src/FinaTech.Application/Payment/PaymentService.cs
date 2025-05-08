2435f9a8ca7a5f69ad8e54db3959f3f93a07db4e
bhamutci <bhamutcu@hotmail.com>
2025-05-08 17:26:03 +0100

    Implement IPaymentService interface

--- src/FinaTech.Application/Payment/PaymentService.cs
+++ src/FinaTech.Application/Payment/PaymentService.cs
@@ -1,6 +1,21 @@
+using FinaTech.Application.Payment.Dto;
+
 namespace FinaTech.Application.Payment;
 
-public class PaymentService: IApplicationService
+public class PaymentService: IPaymentService
 {
+    public Task<PaymentDto> GetPaymentAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
 
+    public Task<PaymentDto> CreatePaymentAsync(PaymentDto payment)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IReadOnlyList<PaymentDto>> GetPaymentsAsync()
+    {
+        throw new NotImplementedException();
+    }
 }
