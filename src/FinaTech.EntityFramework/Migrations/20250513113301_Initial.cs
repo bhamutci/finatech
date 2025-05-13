ca665fed710133d774e7ea0c94103ce8624711a9
bhamutci <bhamutcu@hotmail.com>
2025-05-13 13:08:23 +0100

    Add initial EF Core migration and database setup
    Created tables for Address, Banks, Accounts, and Payments with their relationships and constraints. Updated configuration files to include PostgreSQL connection string, added required Entity Framework Core packages, and referenced the FinaTech.EntityFramework project in the Web Host project.

---
+++ src/FinaTech.EntityFramework/Migrations/20250513113301_Initial.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace FinaTech.EntityFramework.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Address",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    AddressLine1 = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    AddressLine2 = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true),
+                    AddressLine3 = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true),
+                    City = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: true),
+                    PostCode = table.Column<string>(type: "character varying(10)", maxLength: 10, nullable: true),
+                    CountryCode = table.Column<string>(type: "character varying(2)", maxLength: 2, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Address", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Banks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    BIC = table.Column<string>(type: "character varying(11)", maxLength: 11, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Banks", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Accounts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    BankId = table.Column<int>(type: "integer", nullable: false),
+                    AddressId = table.Column<int>(type: "integer", nullable: false),
+                    Name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    AccountNumber = table.Column<string>(type: "character varying(11)", maxLength: 11, nullable: true),
+                    Iban = table.Column<string>(type: "character varying(34)", maxLength: 34, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Accounts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Accounts_Address_AddressId",
+                        column: x => x.AddressId,
+                        principalTable: "Address",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Accounts_Banks_BankId",
+                        column: x => x.BankId,
+                        principalTable: "Banks",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Payments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    OriginatorAccountId = table.Column<int>(type: "integer", nullable: false),
+                    BeneficiaryAccountId = table.Column<int>(type: "integer", nullable: false),
+                    Amount_Amount = table.Column<decimal>(type: "numeric", nullable: false),
+                    Amount_Currency = table.Column<string>(type: "character varying(2)", maxLength: 2, nullable: false),
+                    Date = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    ChargesBearer = table.Column<int>(type: "integer", nullable: false),
+                    Details = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true),
+                    ReferenceNumber = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Payments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Payments_Accounts_BeneficiaryAccountId",
+                        column: x => x.BeneficiaryAccountId,
+                        principalTable: "Accounts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Payments_Accounts_OriginatorAccountId",
+                        column: x => x.OriginatorAccountId,
+                        principalTable: "Accounts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Accounts_AccountNumber",
+                table: "Accounts",
+                column: "AccountNumber");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Accounts_AddressId",
+                table: "Accounts",
+                column: "AddressId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Accounts_BankId",
+                table: "Accounts",
+                column: "BankId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Accounts_Name",
+                table: "Accounts",
+                column: "Name");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Address_AddressLine1",
+                table: "Address",
+                column: "AddressLine1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Address_City",
+                table: "Address",
+                column: "City");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Address_CountryCode",
+                table: "Address",
+                column: "CountryCode");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Address_PostCode",
+                table: "Address",
+                column: "PostCode");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Banks_BIC",
+                table: "Banks",
+                column: "BIC");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Banks_Id",
+                table: "Banks",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Banks_Name",
+                table: "Banks",
+                column: "Name");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Payments_BeneficiaryAccountId",
+                table: "Payments",
+                column: "BeneficiaryAccountId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Payments_OriginatorAccountId",
+                table: "Payments",
+                column: "OriginatorAccountId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Payments_ReferenceNumber",
+                table: "Payments",
+                column: "ReferenceNumber");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Payments");
+
+            migrationBuilder.DropTable(
+                name: "Accounts");
+
+            migrationBuilder.DropTable(
+                name: "Address");
+
+            migrationBuilder.DropTable(
+                name: "Banks");
+        }
+    }
+}
