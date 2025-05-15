6ab1db060a640884fcaf9ab53728d2701b42aa76
bhamutci <bhamutcu@hotmail.com>
2025-05-15 22:05:21 +0100

    Remove Bank entity and refactor Accounts to use BIC directly
    Replaced the Bank relation in Accounts with a direct BIC field, simplifying the data model. Updated migrations, removing the Banks table and related foreign keys, indexes, and references. Added new constraints and indexes on the BIC and IBAN fields in the Accounts table to maintain integrity.

--- src/FinaTech.EntityFramework/Migrations/20250515210441_remove_bank_entity.Designer.cs
+++ src/FinaTech.EntityFramework/Migrations/20250515210441_remove_bank_entity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using FinaTech.EntityFramework.PostgresSqlServer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace FinaTech.EntityFramework.Migrations
 {
     [DbContext(typeof(FinaTechPostgresSqlDbContext))]
-    partial class FinaTechPostgresSqlDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250515210441_remove_bank_entity")]
+    partial class remove_bank_entity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/FinaTech.EntityFramework/Migrations/20250515210441_remove_bank_entity.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace FinaTech.EntityFramework.Migrations
+{
+    /// <inheritdoc />
+    public partial class remove_bank_entity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Accounts_Banks_BankId",
+                table: "Accounts");
+
+            migrationBuilder.DropTable(
+                name: "Banks");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Accounts_BankId",
+                table: "Accounts");
+
+            migrationBuilder.DropColumn(
+                name: "BankId",
+                table: "Accounts");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Bic",
+                table: "Accounts",
+                type: "character varying(11)",
+                maxLength: 11,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Accounts_Bic",
+                table: "Accounts",
+                column: "Bic");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Accounts_Iban",
+                table: "Accounts",
+                column: "Iban");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Accounts_Bic",
+                table: "Accounts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Accounts_Iban",
+                table: "Accounts");
+
+            migrationBuilder.DropColumn(
+                name: "Bic",
+                table: "Accounts");
+
+            migrationBuilder.AddColumn<int>(
+                name: "BankId",
+                table: "Accounts",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Banks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    BIC = table.Column<string>(type: "character varying(11)", maxLength: 11, nullable: false),
+                    Name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Banks", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Accounts_BankId",
+                table: "Accounts",
+                column: "BankId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Banks_BIC",
+                table: "Banks",
+                column: "BIC");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Banks_Id",
+                table: "Banks",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Banks_Name",
+                table: "Banks",
+                column: "Name");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Accounts_Banks_BankId",
+                table: "Accounts",
+                column: "BankId",
+                principalTable: "Banks",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
